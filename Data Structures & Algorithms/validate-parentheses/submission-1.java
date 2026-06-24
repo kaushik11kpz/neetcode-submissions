8690f57d589dd6f8343cec8511bf26c6494abf11
kaushik11kpz <41732566+kaushik11kpz@users.noreply.github.com>
2026-06-25 03:52:47 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+                continue;
+            }
+            else{
+                 if(st.isEmpty()){
+                    return false;
+                 }
+                 if(st.peek()=='(' && c==')'){
+                    st.pop();
+                 }
+                else if(st.peek()=='{' && c=='}'){
+                    st.pop();
+                 }
+                else if(st.peek()=='[' && c==']'){
+                    st.pop();
+                 }
+                else{
+                    return false;
+                 }
+            }
+        }
+        return st.isEmpty();
+    }
+}
